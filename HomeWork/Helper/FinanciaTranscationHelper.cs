e65a2e0d3c9ade62df7881d569ace5ff62f2bc99
Cash Wu <cash@cashwu.com>
2016-08-14 00:58:19 +0800

    change income and expenses font color

---
+++ HomeWork/Helper/FinanciaTranscationHelper.cs
@@ -0,0 +1,15 @@
+﻿using HomeWork.Enum;
+using System.Web.Mvc;
+
+namespace HomeWork.Helper
+{
+    public static class FinanciaTranscationHelper
+    {
+        public static string CategoryStyle(this HtmlHelper helper, CategoryEnum category)
+        {
+            return category == CategoryEnum.支出 
+                ? "fontRed" 
+                : (category == CategoryEnum.收入 ? "fontBlue" : null);
+        }
+    }
+}
