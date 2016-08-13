ba931650b68910a89b0f88d7c664331ce943fefe
Cash Wu <cash@cashwu.com>
2016-08-14 00:14:48 +0800

    chnage view use HtmlHelper

--- HomeWork/Models/FinancialTranscation.cs
+++ HomeWork/Models/FinancialTranscation.cs
@@ -1,5 +1,6 @@
 ﻿using HomeWork.Enum;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HomeWork.Models
 {
@@ -7,14 +8,18 @@ namespace HomeWork.Models
     {
         public int No { get; set; }
 
+        [Display(Name = "類別")]
         public CategoryEnum Category { get; set; }
 
+        [Display(Name = "日期")]
         public DateTime Date { get; set; }
 
+        [Display(Name = "金額")]
         public Decimal Amount { get; set; }
 
         public Guid Guid { get; set; }
 
+        [Display(Name = "備註")]
         public string Remark { get; set; }
     }
 }
